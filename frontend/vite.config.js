c420a7be76b88982c690b7b156d40bc27375d5c1
sohanabhatia <sohanabhatia3@gmail.com>
2026-05-12 14:20:35 +0530

    fix vite allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["prolific-liberation-production.up.railway.app"],
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
